eed3d0e5d2e329b31d583243e47a50b26571ef35
Alex Forrow <alex@forrow.com>
2017-10-10 13:01:33 +0100

    fix load_current_resource for Chef 13

--- providers/service.rb
+++ providers/service.rb
@@ -145,7 +145,7 @@ def get_current_state(service_name)
 end
 
 def load_current_resource
-  @current_resource = Chef::Resource::SupervisorService.new(@new_resource.name)
+  @current_resource = Chef::ResourceResolver.resolve(:supervisor_service).new(@new_resource.name)
   @current_resource.state = get_current_state(@new_resource.name)
 end
 
