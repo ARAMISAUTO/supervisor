4d26d024010e49d582b87d19631b4533f46d968b
Matthieu Serrepuy <lotooo@gmail.com>
2019-03-27 15:11:52 +0100

    Make the cookbook work with latest version of poise-python

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ version           '0.5.0'
 
 recipe 'supervisor', 'Installs and configures supervisord'
 
-depends 'poise-python', '~> 1.6.0'
+depends 'poise-python'
 
 %w( ubuntu debian redhat centos fedora amazon smartos raspbian ).each do |os|
   supports os
